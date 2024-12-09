ec7d1faabacf5e18476cd798c4ee6fd69c727c98
David Paredes <1918862@alu.murciaeduca.es>
2024-12-09 18:55:51 +0100

    ordenado

--- videojuegoBien.js
+++ videojuegoBien.js
@@ -1,31 +1,27 @@
 window.onload = function () {
+  // Variables del DOM
   let puntuacionIzquierda = document.getElementById("puntuacionIzquierda");
   let puntuacionDerecha = document.getElementById("puntuacionDerecha");
   document.getElementById("botonBorrarGanadores").onclick = borrarGanadores;
 
-
+  // Constantes
   const TOPE_INFERIOR = 400; // Altura máxima del canvas
   const TOPE_SUPERIOR = 0;
 
+  // Variables del juego
   let puntosDerecha = 0;
   let puntosIzquierda = 0;
-
   let canvas, ctx;
   let pelota, jugador1, jugador2;
-
   let id;
-
-
   let teclaArriba = false;
   let teclaAbajo = false;
-
   let fpsJuego = 16;
-
   let golpeEnLosLados, GolpeConElJugador, Ganador;
-
   let juegoPausado = false; // Estado del juego
   let estadoJuego = false; // Estado del juego
 
+  // Constructor de la pelota
   function Pelota() {
     this.x = canvas.width / 2;
     this.y = canvas.height / 2;
@@ -43,6 +39,7 @@ window.onload = function () {
     this.tamañoX = 396; // Tamaño X del sprite original
     this.tamañoY = 424; // Tamaño Y del sprite original
     this.posicion = 0; // Frame actual del sprite
+
     this.dibujar = function () {
       ctx.drawImage(
         this.sprite,
@@ -78,6 +75,7 @@ window.onload = function () {
     };
   }
 
+  // Constructor del jugador
   function Jugador(xInicial, yInicial, color, velocidad) {
     this.x = xInicial;
     this.y = yInicial;
@@ -103,6 +101,7 @@ window.onload = function () {
     };
   }
 
+  // Verificar colisiones
   function verificarColisiones() {
     // Colisión con el jugador 1
     if (
@@ -112,7 +111,6 @@ window.onload = function () {
     ) {
       pelota.dx = Math.abs(pelota.dx); // Rebota hacia la derecha
       reproducirAudio(GolpeConElJugador);
-
 
       // Ángulo de rebote según posición del impacto
       let impacto = (pelota.y - (jugador1.y + jugador1.alto / 2)) / (jugador1.alto / 2);
@@ -127,7 +125,6 @@ window.onload = function () {
     ) {
       pelota.dx = -Math.abs(pelota.dx); // Rebota hacia la izquierda
       reproducirAudio(GolpeConElJugador);
-
 
       // Ángulo de rebote según posición del impacto
       let impacto = (pelota.y - (jugador2.y + jugador2.alto / 2)) / (jugador2.alto / 2);
@@ -144,15 +141,17 @@ window.onload = function () {
       puntosIzquierda++;
       actualizarMarcador();
       resetPelota();
-      verificarPuntos(); // Verificar los puntos después de actualizar
+      verificarPuntos(); // Verificar los puntos despu��s de actualizar
     }
   }
+
+  // Actualizar marcador
   function actualizarMarcador() {
     puntuacionIzquierda.innerHTML = `Puntuación Izquierda: ${puntosIzquierda}`;
     puntuacionDerecha.innerHTML = `Puntuación Derecha: ${puntosDerecha}`;
   }
 
-
+  // Actualizar juego
   function actualizarJuego() {
     ctx.clearRect(0, 0, canvas.width, canvas.height); // Limpiar canvas
 
@@ -166,17 +165,15 @@ window.onload = function () {
       jugador2.mover("arriba");
     } else if (pelota.y > jugador2.y + jugador2.alto / 2) {
       jugador2.mover("abajo");
-
     }
-
 
     pelota.dibujar();
     jugador1.dibujar();
     jugador2.dibujar();
     pelota.actualizarAnimacion();
-
   }
 
+  // Resetear pelota
   function resetPelota() {
     pelota.x = canvas.width / 2;
     pelota.y = canvas.height / 2;
@@ -184,36 +181,33 @@ window.onload = function () {
     pelota.dy = 1 * (Math.random() > 0.5 ? 1 : -1);   // Reducir la velocidad vertical
   }
 
-
-
+  // Resetear jugadores
   function resetJugadores() {
     jugador1.y = canvas.height / 2 - jugador1.alto / 2;
     jugador2.y = canvas.height / 2 - jugador2.alto / 2;
   }
 
-
-
+  // Reiniciar juego
   function reiniciarJuego() {
-
     document.getElementById("botonInicio").disabled = true; // Desactivar el botón de inicio
     document.getElementById("botonPausa").disabled = true; // Desactivar el botón de pausa
     document.getElementById("botonPausa").innerText = "Pausar Juego"; // Restablecer el texto del botón de pausa
+
     // Reiniciar las variables a sus valores originales
     puntosIzquierda = 0;
     puntosDerecha = 0;
     teclaArriba = false;
     teclaAbajo = false;
     juegoPausado = false; // Reiniciar el estado del juego
-
     fpsJuego = 16; // Restablecer la velocidad del juego
 
     // Actualizar el marcador
     actualizarMarcador();
     resetJugadores();
     iniciarJuego();
-
   }
 
+  // Verificar puntos
   function verificarPuntos() {
     if (puntosIzquierda === 3 || puntosDerecha === 3) {
       clearInterval(id);
@@ -232,7 +226,7 @@ window.onload = function () {
     }
   }
 
-
+  // Almacenar ganador
   function almacenarGanador(nombre, puntos) {
     let ganadores = JSON.parse(localStorage.getItem("ganadores")) || [];
     ganadores.push({ nombre: nombre, puntos: puntos });
@@ -240,6 +234,7 @@ window.onload = function () {
     localStorage.setItem("ganadores", JSON.stringify(ganadores));
   }
 
+  // Actualizar lista de ganadores
   function actualizarListaGanadores() {
     let ganadores = JSON.parse(localStorage.getItem("ganadores")) || [];
     let listaGanadores = document.getElementById("listaGanadores");
@@ -252,7 +247,7 @@ window.onload = function () {
     });
   }
 
-
+  // Pausar juego
   function pausarJuego() {
     if (juegoPausado) {
       // Reanudar el juego
@@ -266,51 +261,44 @@ window.onload = function () {
     juegoPausado = !juegoPausado;
     document.getElementById("botonInicio").disabled = false; // Activar el botón de reinicio
     document.getElementById("botonInicio").innerText = "Reiniciar Juego"; // Cambiar el texto del botón de inicio
-
   }
 
+  // Activar movimiento
   function activaMovimiento(evt) {
     if (evt.keyCode === 38) teclaArriba = true;
     if (evt.keyCode === 40) teclaAbajo = true;
   }
 
+  // Desactivar movimiento
   function desactivaMovimiento(evt) {
     if (evt.keyCode === 38) teclaArriba = false;
     if (evt.keyCode === 40) teclaAbajo = false;
   }
 
-
-
+  // Reproducir audio
   function reproducirAudio(audio) {
     audio.currentTime = 0;
     audio.play();
   }
 
-
-
-
-
+  // Borrar ganadores
   function borrarGanadores() {
     localStorage.removeItem("ganadores");
     actualizarListaGanadores();
   }
 
-
+  // Botón de inicio
   function botonInicio() {
-
     if (estadoJuego === false) {
-
       iniciarJuego();
       document.getElementById("botonInicio").innerText = "Reiniciar Juego";
-
     } else if (estadoJuego === true) {
-
       reiniciarJuego();
       document.getElementById("botonInicio").innerText = "Iniciar Juego";
-
     }
   }
 
+  // Iniciar juego
   function iniciarJuego() {
     estadoJuego = true; // Iniciar el juego
 
@@ -336,11 +324,8 @@ window.onload = function () {
     actualizarListaGanadores();
   }
 
-
-
-
+  // Inicializar
   document.getElementById("botonPausa").disabled = true; // Activar el botón de pausa
-
 
   // Manejo de teclado
   document.addEventListener("keydown", activaMovimiento);
@@ -348,6 +333,4 @@ window.onload = function () {
 
   document.getElementById("botonInicio").onclick = botonInicio;
   document.getElementById("botonPausa").onclick = pausarJuego;
-
-
 };
